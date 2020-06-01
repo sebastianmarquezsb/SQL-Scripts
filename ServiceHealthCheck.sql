1500c323f6df3efab969bfaf61225684a6cf51c6
Sebastian Marquez <sebastianmarquezsb@gmail.com>
2020-06-01 01:03:49 -0400

    Script MSSQL - Revision estado de Servicios MSSQL Server,SQLServerAgent,SQLBrowser

---
+++ ServiceHealthCheck.sql
@@ -0,0 +1,3 @@
+exec master.dbo.xp_servicecontrol 'QUERYSTATE', 'MSSQLServer'
+exec master.dbo.xp_servicecontrol 'QUERYSTATE', 'SQLServerAgent'
+exec master.dbo.xp_servicecontrol 'QUERYSTATE', 'SQLBrowser'
